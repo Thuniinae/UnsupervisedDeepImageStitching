0b2054d218439cefe0bbd6ef070422bb5ca2a148
Thuniiane <thunia4182@gmail.com>
2022-12-19 22:15:01 +0800

    [add]tf_upgrade_v2 generate Codes for tensorflow2

---
+++ ImageAlignment/Codes_v2/constant.py
@@ -0,0 +1,25 @@
+#training dataset path
+TRAIN_FOLDER = '/app/synth/traning'
+
+#testing dataset path
+TEST_FOLDER = '/app/synth/testing'
+
+#GPU index
+GPU = '0'
+
+#batch size for training
+TRAIN_BATCH_SIZE = 4
+
+#batch size for testing
+TEST_BATCH_SIZE = 1
+
+#num of iters
+ITERATIONS = 1000000
+
+# checkpoints path
+SNAPSHOT_DIR = "./checkpoints_homo"
+
+#sumary path
+SUMMARY_DIR = "./summary"
+
+
