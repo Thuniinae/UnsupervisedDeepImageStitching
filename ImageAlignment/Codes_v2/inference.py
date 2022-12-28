71b0e707f3a220698849311c4d1d2b1c32d8707c
Thuniiane <thunia4182@gmail.com>
2022-12-28 20:28:24 +0800

    [update]api of new version skimage

--- ImageAlignment/Codes_v2/inference.py
+++ ImageAlignment/Codes_v2/inference.py
@@ -73,8 +73,8 @@ with tf.compat.v1.Session(config=config) as sess:
             input2 = input2[0]
             
             # compute psnr/ssim
-            psnr = skimage.measure.compare_psnr(input1*warp_one, warp*warp_one, 255)
-            ssim = skimage.measure.compare_ssim(input1*warp_one, warp*warp_one, data_range=255, multichannel=True)
+            psnr = skimage.metrics.peak_signal_noise_ratio(input1*warp_one, warp*warp_one, data_range=255)
+            ssim = skimage.metrics.structural_similarity(input1*warp_one, warp*warp_one, data_range=255, multichannel=True)
 
             
             print('i = {} / {}, psnr = {:.6f}'.format( i+1, length, psnr))
